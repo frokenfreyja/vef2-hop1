02c9019ff47ac1e032043b7d093537fc6dd31f5f
krimtom <kmt3@hi.is>
2019-03-19 13:22:25 +0000

    js og sql files

---
+++ setup.js
@@ -0,0 +1,43 @@
+require('dotenv').config();
+
+const fs = require('fs');
+const util = require('util');
+
+const { query } = require('./db');
+
+const connectionString = process.env.DATABASE_URL;
+
+const readFileAsync = util.promisify(fs.readFile);
+
+/**
+ * Setur upp gagnagrunn og bætir við gögnum
+ */
+async function main() {
+  console.info(`Set upp gagnagrunn á ${connectionString}`);
+  // droppa töflum ef til
+  await query('DROP TABLE IF EXISTS project');
+  console.info('Töflu eytt');
+
+  // búa til töflur út frá skema
+  try {
+    const createTable = await readFileAsync('./schema.sql');
+    await query(createTable.toString('utf8'));
+    console.info('Tafla búin til');
+  } catch (e) {
+    console.error('Villa við að búa til töflu:', e.message);
+    return;
+  }
+
+  // bæta færslum við töflur
+  try {
+    const insert = await readFileAsync('./insert.sql');
+    await query(insert.toString('utf8'));
+    console.info('Gögnum bætt við');
+  } catch (e) {
+    console.error('Villa við að bæta gögnum við:', e.message);
+  }
+}
+
+main().catch((err) => {
+  console.error(err);
+});
