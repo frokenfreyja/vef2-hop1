2bda2e930fb92327e7c9de396a9689d5fdfd0b57
frokenfreyja <freyjasigur@gmail.com>
2019-03-19 14:08:47 +0000

    toflur

---
+++ schema.sql
@@ -0,0 +1,37 @@
+CREATE TABLE categories (
+  id serial primary key,
+  title varchar(128) not null unique,
+);
+
+CREATE TABLE products (
+  id serial primary key,
+  title varchar(128) not null unique,
+  price int not null,
+  description text not null,
+  image varchar(128),
+  created timestamp with time zone default current_timestamp,
+  foreign key(category) references categories(id),
+);
+
+CREATE TABLE users {
+  id serial primary key, 
+  username varchar(128) not null unique,
+  password varchar(128) not null,
+  admin bit default false, 
+}
+
+CREATE TABLE orders {
+  id serial primary key, 
+  foreign key(user) references users(id),
+  order bit default false,
+  name varchar(128),
+  address varchar(128), 
+  created timestamp with time zone default current_timestamp,
+}
+
+CREATE TABLE cart_products {
+  id serial primary key,
+  foreign key(order) references orders(id),
+  foreign key(product) references products(id),
+  amount int,
+}
